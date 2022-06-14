c1152400b481b65d29b24277bd6b3d1c8ae735a6
BuildTools <unconfigured@null.spigotmc.org>
2022-06-14 12:33:05 +0300

    Task2

--- hw2/Task2/Program.cs
+++ hw2/Task2/Program.cs
@@ -6,7 +6,24 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+                int num;
+                
+                
+                    Console.Write("Введите целое число: ");
+                num = int.Parse(Console.ReadLine());
+
+                num = Math.Abs(num);
+
+                var count = -1;
+                if (num == 0) count = 1;
+                else while ((num / Math.Pow(10, ++count)) >= 1) ;
+
+                Console.WriteLine($"В ведённом числе {count} знаков");
+                Console.Write("Для выхода нажмите любую клавишу...");
+                Console.ReadKey();
+            
         }
     }
+    
 }
