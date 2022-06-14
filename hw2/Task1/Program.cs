4eac33d2fbab328637a78c15811afa2d638985ac
BuildTools <unconfigured@null.spigotmc.org>
2022-06-14 11:54:21 +0300

    Task1

--- hw2/Task1/Program.cs
+++ hw2/Task1/Program.cs
@@ -6,7 +6,31 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите первое число: ");
+            int first = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int second = int.Parse(Console.ReadLine());
+            Console.Write("Введите третье число: ");
+            int third = int.Parse(Console.ReadLine());
+            int max = maxInt(first, second, third);
+            Console.WriteLine($"Максимальное число, которое вы ввели это {max}");
+        }
+
+        static int maxInt(int a, int b, int c)
+        {
+            if (b < a && a > c)
+            {
+                return a;
+            }
+            else if (a < b && b > c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+
         }
     }
 }
